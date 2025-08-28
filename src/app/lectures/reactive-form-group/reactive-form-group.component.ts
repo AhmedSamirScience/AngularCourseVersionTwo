89c92e441fc3c65c48b14a0cd9004d5a38662aa7
CODELABSYS\Ahmed.ElBakrey <ahmed.elbakrey@codelabsys.com>
2025-08-28 15:08:30 +0300

    #58 Master Angular: Understanding setValue, patchValue, valueChanges

--- src/app/lectures/reactive-form-group/reactive-form-group.component.ts
+++ src/app/lectures/reactive-form-group/reactive-form-group.component.ts
@@ -1,11 +1,12 @@
+import { NgClass } from '@angular/common';
 import { Component} from '@angular/core';
 import { FormGroup, ReactiveFormsModule, Validators    } from '@angular/forms';
 import { FormControl } from '@angular/forms';
-
+ 
 @Component({
   selector: 'app-reactive-form-group',
   standalone: true,
-  imports: [ReactiveFormsModule   ],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './reactive-form-group.component.html',
   styleUrl: './reactive-form-group.component.scss'
 })
@@ -21,5 +22,46 @@ export class ReactiveFormGroupComponent {
 
   constructor() {
      console.log(this.userData);
+
+     this.userData.valueChanges.subscribe( (value) => {
+       console.log(value);
+     });
+
+     this.name?.valueChanges.subscribe( (value) => {
+
+       console.log('hhhhhhhhhhhhhhh' + value);
+     });
   }
+
+  get name() {
+    return this.userData.get('name');
+  }
+
+  get email() {
+    return this.userData.get('email');
+  }
+
+  get phone() {
+    return this.userData.get('phone');
+  }
+
+  get address() {
+    return this.userData.get('address');
+  }
+
+  onSubmit() {
+    console.log(this.userData.value);
+    /*this.userData.setValue({
+      name: '',
+      email: '',
+      phone: '',
+      address: ''
+    });*/
+    /*this.userData.patchValue({
+      name: '',
+      email: '',
+    });*/
+    this.userData.reset();
+  }
+
 }
